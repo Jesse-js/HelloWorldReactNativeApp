893ccb5c92a00906efc6751bd524050abb9acbaa
Jesse-js <jesse-jjs@hotmail.com>
2024-02-16 12:13:15 -0300

    feat: add text input

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello Everyone!</Text>
+      <TextInput placeholder="Enter your name" />
       <StatusBar style="auto" />
     </View>
   );
